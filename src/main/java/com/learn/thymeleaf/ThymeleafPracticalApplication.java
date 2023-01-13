f1a2af24a4a5d34bd018c7673b9d11cf2d3b7f88
Akash Gite <giteakash97@gmail.com>
2023-01-13 17:48:59 +0530

    thymelear practical

---
+++ src/main/java/com/learn/thymeleaf/ThymeleafPracticalApplication.java
@@ -0,0 +1,13 @@
+package com.learn.thymeleaf;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafPracticalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafPracticalApplication.class, args);
+		System.out.print("hello");
+	}
+
+}
